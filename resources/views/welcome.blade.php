443ba5ac456830b8d8d025b50a27273c40e50193
AksanAdhi <aksanadhipratama.19@gmail.com>
2024-09-04 21:50:12 +0700

    nambah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,6 +32,7 @@
             @endif
             <h1>Nama : Muhammad Aksan Adhi Pratama</h1>
             <h1>NPM : 2217051097</h1>
+            <h1>C</h1>
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
